95c49d27be9d185ee52ae90c691a8cca405b0d98
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-07-10 14:56:24 +0545

    Added table component

---
+++ resources/views/components/table/th.blade.php
@@ -0,0 +1,3 @@
+<th {{ $attributes->merge(['class' => 'px-6 py-3 text-left border-b border-gray-300 dark:border-gray-600 text-sm font-semibold text-gray-700 dark:text-gray-100']) }}>
+    {{ $slot }}
+</th>

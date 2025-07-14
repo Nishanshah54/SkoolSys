cd5c79831fec46bb591ad9b560588e3c30f07958
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-07-14 17:10:16 +0545

    romove colum

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -8,13 +8,27 @@ use Livewire\WithPagination;
 
 class StudentController extends Controller
 {
-          use WithPagination;
+    use WithPagination;
 
     public function index()
     {
-
-          $students = Student::select('id','name','student_id','mobile_number','education','created_at')->orderByDesc(column: 'id')->paginate(config('skoolsys.paginate_page')??10);
-
+        $students = Student::select('id','name','student_id','mobile_number','education','created_at')->orderByDesc( 'id')->paginate(config('skoolsys.paginate_page') ??10);
          return view('admin.students.index', compact('students'));
+    }
+
+       public function store()
+    {
+        return view('admin.students.add-student');
+    }
+
+    public function show(Student $student)
+    {
+        return view('admin.students.show', compact('student'));
+    }
+
+    public function destroy(Student $student)
+    {
+        $student->delete(); // Soft delete
+        return back()->with('success', "Student trashed successfully with Student ID:{$student->student_id}");
     }
 }

b97f6a0730ba0a01da67579b6bbe798a8a6a46d6
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-07-16 14:14:21 +0545

    Added teacher migration table

---
+++ app/Models/Teacher.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Teacher extends Model
+{
+    /** @use HasFactory<\Database\Factories\Techer\TecherFactory> */
+    use HasFactory,SoftDeletes;
+
+     protected $fillable = [
+        'email',
+        'name',
+        'gender',
+        'dob',
+        'address',
+        'qualification',
+        'phone',
+        'specialization'
+        ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+}

610731b45e60d35cd66e74c4f2059101206f30db
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-08-09 19:54:48 +0545

    added add teacher option in quick bar dashbord

--- resources/views/recentActivities/index.blade.php
+++ resources/views/recentActivities/index.blade.php
@@ -95,10 +95,15 @@
                             </svg>
                             <span class="text-sm font-medium">Add Student</span>
                         </a>
+                        <a href="{{route('admin.teacher.create')}}"
+                            class="p-3 rounded-lg border border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-800 transition flex flex-col items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-plus-icon lucide-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><line x1="19" x2="19" y1="8" y2="14"/><line x1="22" x2="16" y1="11" y2="11"/></svg>
+                            <span class= text-sm font-medium">Add Teacher</span>
+                        </a>
 
                         <a href="{{route('schedules.index')}}"
                             class="p-3 rounded-lg border  border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-800 transition flex flex-col items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-check-icon lucide-calendar-check text-green-600"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/><path d="m9 16 2 2 4-4"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-check-icon lucide-calendar-check text-green-600 pt-3"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/><path d="m9 16 2 2 4-4"/></svg>
                             <span class="text-sm font-medium pt-3">Create Schedule</span>
                         </a>
                         <a href="{{route('schedules.index')}}"

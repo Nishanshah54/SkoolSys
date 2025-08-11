046f91e1f0ee5fa46dfec3ffe703c442384b7ecb
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-08-11 20:45:37 +0545

    added dymanic app name of system in footer

--- resources/views/components/layouts/guest/header.blade.php
+++ resources/views/components/layouts/guest/header.blade.php
@@ -4,12 +4,12 @@
         @include('partials.head')
     </head>
         <body class="bg-[#4c7cd4] dark:bg-[#0a0a0a] text-[#eeeee9] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-      
+
         {{ $slot }}
     <footer class="mt-8 text-sm   w-full py-2 px-2  dark:bg-[#0a0a0a] text-center text-[#cccccc]">
        <span> Developed by ❤️Nishan shah | Version {{ config('skoolsys.version') }}</span> <br>
-    <span>Copyright @ 2024 - {{ now()->year }} | SKoolSys | All Rights Reserved </span>
+    <span>Copyright @ 2024 - {{ now()->year }} | {{env('APP_NAME')}} | All Rights Reserved </span>
     </footer>
-    </body>    
+    </body>
 </html>
         
